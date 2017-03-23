a5593a89c816ff26bba8085e93429af4e3b55bab
YuzhongHuang <yuzhong.huang@students.olin.edu>
2017-03-22 20:46:55 -0400

    fix spaces/tab issue

--- src/scripts/network.py
+++ src/scripts/network.py
@@ -4,50 +4,50 @@ import tensorflow as tf
 
 class DQN(object):
 
-	def __init__(self, lr, y, e):
-		# hyper parameters
-		self.lr = lr 	# learning rate
-		self.y = y
-		self.e = e 		# random action probablity
-		self.i = 0
-		# These lines establish the feed-forward part of the network
-		# used to choose actions
-		self.inputs1 = tf.reshape(tf.placeholder(shape=[32,32],dtype=tf.float32), [1,1024])
-		self.W = tf.Variable(tf.random_uniform([1024,3],0,0.01))
-		self.output = tf.matmul(self.input, self.W)
-		self.predict = tf.argmax(self.output, 1)
+    def __init__(self, lr, y, e):
+        # hyper parameters
+        self.lr = lr    # learning rate
+        self.y = y
+        self.e = e      # random action probablity
+        self.i = 0
+        # These lines establish the feed-forward part of the network
+        # used to choose actions
+        self.inputs1 = tf.reshape(tf.placeholder(shape=[32,32],dtype=tf.float32), [1,1024])
+        self.W = tf.Variable(tf.random_uniform([1024,3],0,0.01))
+        self.output = tf.matmul(self.input, self.W)
+        self.predict = tf.argmax(self.output, 1)
 
-		# Below we obtain the loss by taking the sum of squares
-		# difference between the target and prediction Q values.
-		self.nextQ = tf.placeholder(shape=[1,3],dtype=tf.float32)
-		self.loss = tf.reduce_sum(tf.square(nextQ - Qout))
-		self.trainer = tf.train.GradientDescentOptimizer(learning_rate=self.lr)
-		self.updateModel = trainer.minimize(loss)
+        # Below we obtain the loss by taking the sum of squares
+        # difference between the target and prediction Q values.
+        self.nextQ = tf.placeholder(shape=[1,3],dtype=tf.float32)
+        self.loss = tf.reduce_sum(tf.square(nextQ - Qout))
+        self.trainer = tf.train.GradientDescentOptimizer(learning_rate=self.lr)
+        self.updateModel = trainer.minimize(loss)
 
-		# initialize session
-		self.init = tf.initialize_all_variables()
-		self.sess = tf.Session()
+        # initialize session
+        self.init = tf.initialize_all_variables()
+        self.sess = tf.Session()
 
-		# action set
-		self.actions = [0, 1, 2] # TODO: change to enum
+        # action set
+        self.actions = [0, 1, 2] # TODO: change to enum
 
-		#weights to calculate the reward
-		self.weights = np.arange(31.0, -1.0, -1.0)
+        #weights to calculate the reward
+        self.weights = np.arange(31.0, -1.0, -1.0)
         self.weights = self.weights/np.sum(self.weights)
 
-	def start(self):
-		"""start a session"""
-		self.sess.run(self.init)
+    def start(self):
+        """start a session"""
+        self.sess.run(self.init)
 
-	def stop(self):
-		"""end a session"""
-		self.sess.close()
+    def stop(self):
+        """end a session"""
+        self.sess.close()
 
-	def update(self, state):
-		""" compute loss and update the network with next state and reward"""
+    def update(self, state):
+        """ compute loss and update the network with next state and reward"""
 
-		reward = self.calculate_reward(state, self.weights)
-		# obtain the Q' values by feeding the new state through the network
+        reward = self.calculate_reward(state, self.weights)
+        # obtain the Q' values by feeding the new state through the network
 
         Q1 = sess.run(self.output,feed_dict={self.input:state})
         max_Q1 = np.max(Q1)
@@ -70,13 +70,13 @@ class DQN(object):
         if np.random.rand(1) < e:
             self.a[0] = self.get_random_action()
 
-		self.i += 1
-		self.e = 1./((self.i/50.0) + 10)
+        self.i += 1
+        self.e = 1./((self.i/50.0) + 10)
 
         return self.a, self.Q
 
-	def calculate_reward(self, binary, weights):
-		"""calculates the reward from the image"""
+    def calculate_reward(self, binary, weights):
+        """calculates the reward from the image"""
 
         goodness = 0
         for r in range(len(binary)-1, -1, -1):
